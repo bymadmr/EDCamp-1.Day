77848b6cc546775da6eef55af7b64ed79bd2314c
Muhammed <bymadmr@gmail.com>
2022-09-02 04:12:17 +0300

    3. lesson completed.

---
+++ src/demoProjects/rentACar/Application/Features/Brands/Queries/GetListBrand/GetListBrandQuery.cs
@@ -0,0 +1,38 @@
+﻿using Application.Features.Brands.Models;
+using Application.Services.Repositories;
+using AutoMapper;
+using Core.Application.Requests;
+using Core.Persistence.Paging;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Queries.GetListBrand
+{
+    public class GetListBrandQuery : IRequest<BrandListModel>
+    {
+        public PageRequest PageRequest { get; set; }
+        public class GetListBrandQueryHandler : IRequestHandler<GetListBrandQuery, BrandListModel>
+        {
+
+            IBrandRepository _brandRepository;
+            IMapper _mapper;
+            public GetListBrandQueryHandler(IMapper mapper, IBrandRepository brandRepository)
+            {
+                _mapper = mapper;
+                _brandRepository = brandRepository;
+            }
+            public async Task<BrandListModel> Handle(GetListBrandQuery request, CancellationToken cancellationToken)
+            {
+                IPaginate<Brand> languages = await _brandRepository.GetListAsync(index: request.PageRequest.Page, size: request.PageRequest.PageSize);
+                BrandListModel mappedLanguageListModel = _mapper.Map<BrandListModel>(languages);
+                return mappedLanguageListModel;
+            }
+        }
+
+    }
+}
